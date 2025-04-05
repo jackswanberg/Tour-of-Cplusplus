5f0a7f5952a2a4e9aa84ffc7c8eaf538b72ffe4a
Jack Swanberg <swanb045@umn.edu>
2025-04-05 08:03:48 -0500

    Added entry header file

--- Ch12/containers.cpp
+++ Ch12/containers.cpp
@@ -2,55 +2,57 @@
 #include <string>
 #include <iostream>
 
+#include "entry.h"
+
 using namespace std;
 
-struct Entry{
-    string name;
-    int number;
-};
+// struct Entry{
+//     string name;
+//     int number;
+// };
 
-ostream& operator<<(ostream& os, const Entry& e)
-{
-    return os << "{\"" << e.name << "\", " << e.number << "}";
-}
+// ostream& operator<<(ostream& os, const Entry& e)
+// {
+//     return os << "{\"" << e.name << "\", " << e.number << "}";
+// }
 
-istream& operator>>(istream& is, Entry& e)
-{
-    // read {"NAME", NUMBER} pair. formatted with {"", and }
-    char c, c2;
-    if(is>>c && c=='{' && is>>c2 && c2=='"'){
-        string name;
-        while(is.get(c)&&c!='"')
-            name+=c;
+// istream& operator>>(istream& is, Entry& e)
+// {
+//     // read {"NAME", NUMBER} pair. formatted with {"", and }
+//     char c, c2;
+//     if(is>>c && c=='{' && is>>c2 && c2=='"'){
+//         string name;
+//         while(is.get(c)&&c!='"')
+//             name+=c;
 
-        if(is>>c && c==','){
-            int number = 0;
-            if(is>>number>>c && c=='}'){
-                e={name,number};
-                return is;
-            }
-        }
-    }
-    is.setstate(ios_base::failbit);
-    return is;
-}
+//         if(is>>c && c==','){
+//             int number = 0;
+//             if(is>>number>>c && c=='}'){
+//                 e={name,number};
+//                 return is;
+//             }
+//         }
+//     }
+//     is.setstate(ios_base::failbit);
+//     return is;
+// }
 
-void print_book(const vector<Entry>& book)
-{
-    // Const means we will not be changing the value
-    // auto lets the compiler determine what type it is
-    // & Means we want a reference of the item, not a copy
-    for(const auto& item : book){
-        std::cout << item << "\n";
-    }
-}
+// void print_book(const vector<Entry>& book)
+// {
+//     // Const means we will not be changing the value
+//     // auto lets the compiler determine what type it is
+//     // & Means we want a reference of the item, not a copy
+//     for(const auto& item : book){
+//         std::cout << item << "\n";
+//     }
+// }
 
-//Need to have the reference in order for it to affect the values in main
-void input_entry(vector<Entry>& book){
-    for(Entry e; cin>>e;){
-        book.push_back(e);
-    }
-}
+// //Need to have the reference in order for it to affect the values in main
+// void input_entry(vector<Entry>& book){
+//     for(Entry e; cin>>e;){
+//         book.push_back(e);
+//     }
+// }
 
 int main(){
     vector<Entry> phone_book = {
